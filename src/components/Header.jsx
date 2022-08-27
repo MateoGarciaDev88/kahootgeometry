c3a4d96655ae465021c249496b41800896eecea0
Mateo Garcia <mgarciaz@geosat.com.co>
2022-08-27 14:39:38 -0500

    Creacion card de la preguntas, botones de seleccion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-        <h1 className='font-black text-5xl text-center lg:w-2/3 md:w-1/2 mx-auto'>
-            Previa
-            <span className='text-indigo-900'> Geometria</span>
+        <h1 className='font-black text-5xl text-center lg:w-2/3 md:w-1/2 mx-auto uppercase'>
+            <span className='text-[#83b2fd]'>Previa</span>
+            <span className='text-[#c9daf8]'> Geometria</span>
         </h1>
     </div>
   )
--- src/components/Puntacion.jsx
+++ src/components/Puntacion.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Puntacion = () => {
   return (
-    <div className='md:w-1/2 lg:w-3/5'>
+    <div className='md:w-1/2 lg:w-1/6'>
         Puntacion
     </div>
   )
