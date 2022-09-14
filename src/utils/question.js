bfb0b3f2a09f5763c6d5c6267f9c21b4997e6359
Mateo Garcia <mgarciaz@geosat.com.co>
2022-09-14 18:35:28 -0500

    links preguntas

--- src/utils/question.js
+++ src/utils/question.js
@@ -12,7 +12,7 @@ export const questions = [
     },
     // 2
     {
-        questionvideo: 'https://www.youtube.com/watch?v=pLBuFxMYkx8',
+        questionvideo: 'https://youtu.be/G_PJVBzGa9k ',
         questionImage: 'src/assets/question_2.png',
         answerOptions: [
             {answerText: 'A', isCorrect: false },
@@ -23,7 +23,7 @@ export const questions = [
     },
     // 3
     {
-        questionvideo: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat',
+        questionvideo: 'https://youtu.be/1SREjabJUOA',
         questionImage: 'src/assets/question_3.png',
         answerOptions: [
             {answerText: 'A', isCorrect: false },
@@ -34,7 +34,7 @@ export const questions = [
     },
     // 4
     {
-        questionvideo: 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.',
+        questionvideo: 'https://youtu.be/n9yffRiJFAo',
         questionImage: 'src/assets/question_4.png',
         answerOptions: [
             {answerText: 'A', isCorrect: false },
@@ -45,7 +45,7 @@ export const questions = [
     },
     // 5
     {
-        questionvideo: 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.',
+        questionvideo: 'https://www.youtube.com/watch?v=m9Q_coOCQB0',
         questionImage: 'src/assets/question_5.png',
         answerOptions: [
             {answerText: 'A', isCorrect: false },
@@ -56,7 +56,7 @@ export const questions = [
     },
     // 6
     {
-        questionvideo: 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.',
+        questionvideo: 'https://www.youtube.com/watch?v=UeY-RWPSMq8',
         questionImage: 'src/assets/question_6.png',
         answerOptions: [
             {answerText: 'A', isCorrect: false },
@@ -67,7 +67,7 @@ export const questions = [
     },
     // 7
     {
-        questionvideo: 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.',
+        questionvideo: 'https://www.youtube.com/watch?v=00ZXRRTnJ9w',
         questionImage: 'src/assets/question_7.png',
         answerOptions: [
             {answerText: 'A', isCorrect: false },
